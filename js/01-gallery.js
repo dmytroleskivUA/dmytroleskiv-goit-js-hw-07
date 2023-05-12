4af93a65e04abc37e96bf9c906e70a19b81eec83
dmytroleskivUA <121440440+dmytroleskivUA@users.noreply.github.com>
2023-05-12 17:06:51 +0100

    1 task done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,52 @@
 import { galleryItems } from "./gallery-items.js";
-// Change code below this line
 
-console.log(galleryItems);
+// Cтворення розмітки
+
+const gallery = document.querySelector(".gallery");
+
+const li = galleryItems
+  .map((item) => {
+    return `<li class="gallery__item">
+    <a class="gallery__link" href="${item.original}" >
+    <img
+    class="gallery__image"
+    src="${item.preview}"
+    data-source="${item.original}"
+    alt="${item.description}"
+    />
+    </a>
+    </li>`;
+  })
+  .join("\n");
+
+galleryList.insertAdjacentHTML("afterbegin", li);
+
+// Заборона перенаправлення для посилань
+
+document
+  .querySelectorAll(".gallery__link")
+  .forEach((item) =>
+    item.addEventListener("click", (event) => event.preventDefault())
+  );
+
+//   Click на елементи галереї
+
+function openFullImage(event) {
+  if (event.target.nodeName !== "IMG") return;
+
+  const fullImageUrl = event.target.dataset.source;
+  const instance = basicLightbox.create(
+    `<img src="${fullImageUrl}" width="800" height="600">`
+  );
+
+  instance.show();
+
+  window.addEventListener("keydown", function closeFullImage(event) {
+    if (event.key === "Escape") {
+      instance.close();
+      window.removeEventListener("keydown", closeFullImage);
+    }
+  });
+}
+
+galleryList.addEventListener("click", openFullImage);
